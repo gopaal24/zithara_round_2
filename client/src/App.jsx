38a98b7b361a33ceb704e4ed0e0877f3c5e88a98
A Yodha Gopal <gopaal.akkineni@gmail.com>
2024-03-01 14:38:20 +0530

    Created form to add records

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import InputTodo from './components/InputTodo'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>Hello World</h1>
+      <InputTodo/>
     </>
   )
 }
